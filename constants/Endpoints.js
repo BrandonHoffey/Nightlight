af30ba4538a6871f7e2a41c48dd0fe8e165ace77
Brandon Hoffey <brandon.hoffey@yahoo.com>
2023-11-21 08:39:56 -0700

    Made changes, some stuff still broken

--- constants/Endpoints.js
+++ constants/Endpoints.js
@@ -1,26 +1,26 @@
 export const API_GROUP_ADD =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/group/add";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/group/add";
 export const API_GROUP_VIEW_ALL =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/group/viewAll";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/group/viewAll";
 export const API_VIEW_ALL_USERS =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/user/view-all";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/user/view-all";
 export const API_USER_SIGN_IN =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/user/sign-in";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/user/sign-in";
 export const API_USER_SIGN_UP =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/user/sign-up";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/user/sign-up";
 export const API_MESSAGE_SEND =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/message/create";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/message/create";
 export const API_MESSAGE_VIEW =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/message/:id/view";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/message/:id/view";
 export const API_VIEW_ALL_FRIENDS =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/friend/view-all";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/friend/view-all";
 export const API_SEND_FRIEND_REQUEST =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/friend/add";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/friend/add";
 export const API_FRIEND_REQUESTS =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/friend/friend-requests";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/friend/friend-requests";
 export const API_FRIEND_REQUEST_ACCEPT =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/friend/friend-request/accept";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/friend/friend-request/accept";
 export const API_SENT_FRIEND_REQUESTS =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/friend/friend-requests/sent";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/friend/friend-requests/sent";
 export const API_VIEW_ALL_FRIENDS_BY_ID =
-  "https://9a35-2601-282-4303-1fc0-7089-faab-d4e-cdb5.ngrok-free.app/friend/friends";
+  "https://4ec8-2601-282-4303-1fc0-6532-bcff-e751-4767.ngrok-free.app/friend/friends";
