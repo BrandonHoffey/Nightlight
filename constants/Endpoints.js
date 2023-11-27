87fec544956fe1120d2adfa5a1fab9ea4fa299ac
Brandon Hoffey <brandon.hoffey@yahoo.com>
2023-11-27 07:18:16 -0700

    Single endpoint variable

--- constants/Endpoints.js
+++ constants/Endpoints.js
@@ -1,30 +1,17 @@
-export const API_GROUP_ADD =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/group/add";
-export const API_GROUP_VIEW_ALL =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/group/viewAll";
-export const API_VIEW_ALL_USERS =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/user/view-all";
-export const API_USER_SIGN_IN =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/user/sign-in";
-export const API_USER_SIGN_UP =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/user/sign-up";
-export const API_MESSAGE_SEND =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/message/create";
-export const API_MESSAGE_VIEW =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/message/:id/view";
-export const API_VIEW_ALL_FRIENDS =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/friend/view-all";
-export const API_SEND_FRIEND_REQUEST =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/friend/add";
-export const API_FRIEND_REQUESTS =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/friend/friend-requests";
-export const API_FRIEND_REQUEST_ACCEPT =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/friend/friend-request/accept";
-export const API_SENT_FRIEND_REQUESTS =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/friend/friend-requests/sent";
-export const API_VIEW_ALL_FRIENDS_BY_ID =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/friend/friends";
-export const API_USER_EDIT_BY_ID =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/user/edit-account";
-export const API_UPDATE_STATUS =
-  "https://f98f-2601-282-4303-1fc0-acab-99c9-dcce-6b19.ngrok-free.app/user/update-status";
+const NGROK = "https://6808-2601-282-4303-1fc0-1c35-8055-a041-2fd3.ngrok-free.app/";
+
+export const API_GROUP_ADD = `${NGROK}group/add`;
+export const API_GROUP_VIEW_ALL = `${NGROK}group/viewAll`;
+export const API_VIEW_ALL_USERS = `${NGROK}user/view-all`;
+export const API_USER_SIGN_IN = `${NGROK}user/sign-in`;
+export const API_USER_SIGN_UP = `${NGROK}user/sign-up`;
+export const API_MESSAGE_SEND = `${NGROK}message/create`;
+export const API_MESSAGE_VIEW = `${NGROK}message/:id/view`;
+export const API_VIEW_ALL_FRIENDS = `${NGROK}friend/view-all`;
+export const API_SEND_FRIEND_REQUEST = `${NGROK}friend/add`;
+export const API_FRIEND_REQUESTS = `${NGROK}friend/friend-requests`;
+export const API_FRIEND_REQUEST_ACCEPT = `${NGROK}friend/friend-request/accept`;
+export const API_SENT_FRIEND_REQUESTS = `${NGROK}friend/friend-requests/sent`;
+export const API_VIEW_ALL_FRIENDS_BY_ID = `${NGROK}friend/friends`;
+export const API_USER_EDIT_BY_ID = `${NGROK}user/edit-account`;
+export const API_UPDATE_STATUS = `${NGROK}user/update-status`;
