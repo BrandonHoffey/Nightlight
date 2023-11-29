d0d781cbdac1dd46ce93092be89a9fd81a9ceef1
Brandon Hoffey <brandon.hoffey@yahoo.com>
2023-11-29 08:16:39 -0700

    Decline Request button

--- screens/friends/components/FriendRequest.js
+++ screens/friends/components/FriendRequest.js
@@ -10,7 +10,7 @@ import React, { useContext } from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import { UserContext } from "../../../UserContext";
 import { useNavigation } from "@react-navigation/core";
-import { API_FRIEND_REQUEST_ACCEPT } from "../../../constants/Endpoints";
+import { API_FRIEND_REQUEST_ACCEPT, API_FRIEND_REQUEST_DECLINE } from "../../../constants/Endpoints";
 import Colors from "../../../Colors";
 
 const fontScale = PixelRatio.getFontScale();
@@ -46,46 +46,84 @@ const FriendRequest = ({ item, FriendRequests, setFriendRequests }) => {
           console.log("error accepting the friend request", error);
         }
       };
-     
   
-  return (
-    <Pressable
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginVertical: 10,
-      }}
-    >
-      <Image
-        style={{ width: 50, height: 50, borderRadius: 25 }}
-        source={{ uri: item.profilePicture }}
-      />
-      <Text
-        style={{
-          fontSize: getFontSize(16),
-          fontWeight: "bold",
-          marginLeft: 10,
-          flex: 1,
-          color: "white",
-        }}
-      >
-        {item?.displayName} sent you a friend request!!
-      </Text>
-      <Pressable
-        onPress={() => acceptRequest(item._id)}
-        style={{
-          backgroundColor: Colors.lightBlue,
-          padding: 10,
-          borderRadius: 20,
-        }}
-        >
-        <Text style={{ textAlign: "center", color: "white" }}>Accept</Text>
-      </Pressable>
-    </Pressable>
+      const declineRequest = async (friendRequestId) => {
+        try {
+          const response = await fetch(API_FRIEND_REQUEST_DECLINE, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: token,
+            },
+            body: JSON.stringify({
+              senderId: friendRequestId,
+              recipientId: userId,
+            }),
+          });
     
-  );
-};
+          const responseData = await response.json();
+          if (response.ok) {
+            setFriendRequests((prevFriendRequests) =>
+              prevFriendRequests.filter(
+                (request) => request._id !== friendRequestId
+              )
+            );
+          }
+        } catch (error) {
+          console.log("error declining the friend request", error);
+        }
+      };
+  
+      return (
+        <View>
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginVertical: 10,
+            }}
+          >
+            <Image
+              style={{ width: 50, height: 50, borderRadius: 25 }}
+              source={{ uri: item.profilePicture }}
+            />
+            <Text
+              style={{
+                fontSize: getFontSize(16),
+                fontWeight: "bold",
+                marginLeft: 10,
+                flex: 1,
+                color: "white",
+              }}
+            >
+              {item?.displayName} sent you a friend request!!
+            </Text>
+            <Pressable
+              onPress={() => acceptRequest(item._id)}
+              style={{
+                backgroundColor: Colors.lightBlue,
+                padding: 10,
+                borderRadius: 20,
+              }}
+            >
+              <Text style={{ textAlign: "center", color: "white" }}>Accept</Text>
+            </Pressable>
+            <Pressable
+              onPress={() => declineRequest(item._id)}
+              style={{
+                backgroundColor: Colors.red, // Choose the color for the "Decline" button
+                padding: 10,
+                borderRadius: 20,
+                marginLeft: 10,
+              }}
+            >
+              <Text style={{ textAlign: "center", color: "white" }}>Decline</Text>
+            </Pressable>
+          </Pressable>
+        </View>
+      );
+    };
 
 export default FriendRequest;
 
